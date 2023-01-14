bd4280e8656c934e7fcd339f037654ce23538e8d
Azuredrak0 <122696665+Azuredrak0@users.noreply.github.com>
2023-01-14 15:15:47 -0500

    Update SmartBuff.lua

--- SmartBuff.lua
+++ SmartBuff.lua
@@ -717,7 +717,7 @@ function SMARTBUFF_SetBuffs()
     for _, buff in SMARTBUFF_SHAMAN  do
       n = SMARTBUFF_SetBuff(buff, n);
     end
-    cPatterns = SMARTBUFF_MAGE_PATTERN;
+    cPatterns = SMARTBUFF_SHAMAN_PATTERN;
     
   elseif (sPlayerClass == "WARRIOR") then
     for _, buff in SMARTBUFF_WARRIOR  do
